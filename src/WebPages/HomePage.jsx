ebebbd5b2efe5101e9bc338f1a43e32d4c56b8e8
I-stack-glitch <ff2974247@gmail.com>
2025-11-23 16:54:17 +0400

    Initial Commit

--- src/WebPages/HomePage.jsx
+++ src/WebPages/HomePage.jsx
@@ -342,7 +342,7 @@ const HomePage = () => {
                     <div className="HomePageOurTopics">
                         <div className="TeamMates">
                             <div className="TeamMateImages">
-                                <div className="Imageone" data-image="https://k72.ca/uploads/teamMembers/MyleneS_480x640-480x640.jpg">
+                                <div className="Imageone" data-image="https://tse3.mm.bing.net/th/id/OIP.1kC1iKZPJu30vjMzA6jF5gHaHg?rs=1&pid=ImgDetMain&o=7&rm=3">
                                 </div>
                                 <div className="Imageone" data-image="https://imageio.forbes.com/specials-images/imageserve/5b8576db31358e0429c734e3/0x0.jpg?format=jpg&crop=2170,2172,x211,y900,safe&height=416&width=416&fit=bounds">
                                 </div>
