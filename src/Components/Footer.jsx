ed284df4e8b6fa230516fe39f21b0a88c94d1d42
I-stack-glitch <ff2974247@gmail.com>
2025-11-23 17:29:43 +0400

    Scroll To Top

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import FooterImg from "./ChatGPT Image Nov 23, 2025, 03_12_23 PM.png"
 import "../ComponentStyling/Footer.css"
+import { ScrollSmoother } from 'gsap/all'
 
 const Footer = () => {
+    const scrollToTop = () => {
+        const smoother = ScrollSmoother.get()
+        if (smoother) {
+            smoother.scrollTo(0, true, "power2.out")
+        } else {
+            // Fallback for if ScrollSmoother isn't available
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            })
+        }
+    }
+
     return (
         <>
             <div className="Footer">
@@ -14,7 +28,7 @@ const Footer = () => {
                         <h1>S<h2>YMMETRIA</h2></h1>
                     </div>
                     <div className="RightFooterContainerEnd">
-                        <h1>SCROLL TO TOP <svg width="13" height="13" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <h1 onClick={scrollToTop} style={{cursor: 'pointer'}}>SCROLL TO TOP <svg width="13" height="13" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M11 8L15 4M15 4L19 8M15 4L15 17C15 18.6569 13.6569 20 12 20H5" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
                         </svg></h1>
 
